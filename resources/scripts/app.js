7871db08ab74e684fce7be2f7047132338e49014
tobirick <tobirickmann@gmail.com>
2018-02-14 14:51:42 +0100

    add sidebar status to localstorage to save settings

--- resources/scripts/app.js
+++ resources/scripts/app.js
@@ -1,6 +1,18 @@
 const toggleSidebarEl = document.querySelector('.main-content__toggle-sidebar');
 
-toggleSidebarEl.addEventListener('click', () => {
+window.onload = () => {
+    const sidebarCurrentStatus = localStorage.getItem('sidebar-closed');
+    if(JSON.parse(sidebarCurrentStatus)) {
+        toggleSidebarEl.classList.add('open');
+        document.body.classList.add('sidebar-closed');
+    }
+};
+
+const toggleSidebar = () => {
     toggleSidebarEl.classList.toggle('open');
-    document.body.classList.toggle('sidebar-open');
-});
+    document.body.classList.toggle('sidebar-closed');
+    const currentStatus = document.body.classList.contains('sidebar-closed') ? true : false;
+    localStorage.setItem('sidebar-closed', currentStatus);
+};
+
+toggleSidebarEl.addEventListener('click', toggleSidebar);
