608e0519020f8d79e1a878f1f9cd498d9a72b5fb
tobirick <tobirickmann@gmail.com>
2018-03-30 23:09:12 +0200

    fixed language redirect bug

--- App/Controllers/DefaultPageController.php
+++ App/Controllers/DefaultPageController.php
@@ -18,6 +18,11 @@ class DefaultPageController extends BaseController {
           self::publicRedirect('/');
       }
 
+      if(!$args['page-args']) {
+         self::publicRedirect('/');
+         exit;
+      }
+
       $activeTheme = Theme::getActiveTheme();
       self::render('public/themes/' . $activeTheme['name'] . '/default-page', $args['page-args']);
     }
