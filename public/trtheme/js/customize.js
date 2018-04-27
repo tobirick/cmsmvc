5f5db319f06957363bcd275a280e7aa22dff371a
tobirick <tobirickmann@gmail.com>
2018-04-27 16:32:25 +0200

    remove files from git repository

--- public/trtheme/js/customize.js
+++
@@ -1,1 +0,0 @@
-console.log('Your CMS is running!');
--- public/trtheme/js/default/langswitcher.js
+++
@@ -1,8 +0,0 @@
-var changeLangEl = document.querySelector('#changePublicLang');
-var currentLang = changeLangEl.value;
-var changeLanguage = function(e) {
-   var newLang = changeLangEl.value;
-   window.location.href = window.location.origin + '/' + newLang;
-}
-
-changeLangEl.addEventListener('change', changeLanguage);
